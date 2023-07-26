3432265d0cf8c5197ce60393217ad6b2433650b4
tgpaulos <warsidotigist11@gmail.com>
2023-07-26 08:37:24 -0700

    push updates

---
+++ get_env.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+char *get_env_value(const char *name) {
+    return getenv(name);
+}
+
+char *build_command_path(const char *command, const char *directory) {
+    size_t cmd_len = strlen(command);
+    size_t dir_len = strlen(directory);
+    size_t len = cmd_len + dir_len + 2;
+    char *cmd = malloc(len);
+    if (cmd == NULL) {
+        return NULL;
+    }
+    char *p = cmd;
+    for (size_t i = 0; i < dir_len; i++) {
+        *p++ = directory[i];
+    }
+    *p++ = '/';
+    for (size_t i = 0; i < cmd_len; i++) {
+        *p++ = command[i];
+    }
+    *p = '\0';
+    return cmd;
+}
+
+int search_path_for_command(char **command) {
+    char *path = get_env_value("PATH");
+    char *value = strtok(path, ":");
+    while (value != NULL) {
+        char *cmd_path = build_command_path(*command, value);
+        if (access(cmd_path, F_OK) == 0) {
+            *command = strdup(cmd_path);
+            free(cmd_path);
+            free(path);
+            return 0;
+        }
+        free(cmd_path);
+        value = strtok(NULL, ":");
+    }
+    free(path);
+    return 1;
+}
+
+int main() {
+    char *command = "ls";
+    int result = search_path_for_command(&command);
+    if (result == 0) {
+        // Command found: Do something else instead of printf
+    } else {
+        // Command not found: Do something else instead of printf
+    }
+    free(command);
+    return 0;
+}
+
---
+++ get_path.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern char **environ;
+
+int string_length(const char *str) {
+        int length = 0;
+        while (str[length] != '\0') {
+                length++;
+        }
+        return length;
+}
+
+int string_compare(const char *str1, const char *str2) {
+        int i = 0;
+        while (str1[i] != '\0' && str2[i] != '\0') {
+                if (str1[i] != str2[i]) {
+                        return 0;
+                }
+                i++;
+        }
+        return (str1[i] == '\0' && str2[i] == '\0');
+}
+
+void display_path() {
+        char *path = NULL;
+        int i = 0;
+
+        while (environ[i] != NULL) {
+                if (string_compare(environ[i], "PATH=")) {
+                        path = environ[i] + 5;
+                        break;
+                }
+                i++;
+        }
+
+        if (path != NULL) {
+                printf("%s\n", path);
+        }
+        if (path != NULL) {
+                printf("%s\n", path);
+        }
+}
+
+int main() {
+        display_path();
+        return 0;
+}
